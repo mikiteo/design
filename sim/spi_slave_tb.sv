016e3c55cca8902f7088a8f1490ff5438cdd86c2
Nikita Biriuk <109039042+mikiteo@users.noreply.github.com>
2025-03-13 16:13:50 +0200

    Update spi_slave_tb.sv

--- sim/spi_slave_tb.sv
+++ sim/spi_slave_tb.sv
@@ -2,7 +2,6 @@
 
 module tb_spi_slave;
 
-    // Тактовий сигнал (50 MHz)
     reg clk = 0;
     reg cs = 1;
     reg sclk = 0;
@@ -15,7 +14,6 @@ module tb_spi_slave;
     reg [7:0] fake_data = 8'b11001100;
     integer i;
 
-    // Інстанціювання тестованого модуля
     spi_slave uut (
         .clk(clk),
         .sclk(sclk),
@@ -37,18 +35,14 @@ module tb_spi_slave;
         $display("time=%0t | bit_cnt: %d", $time, uut.bit_cnt);
     end
 
-    // Основний тест
     initial begin
 
-        // Ініціалізація
         cs = 1;
         mosi = 0;
         sclk = 0;
 
-        // Активуємо SPI Slave
         #10 cs = 0;
 
-        // Передаємо 8 біт на MOSI
         for (i = 0; i < 8; i = i + 1) begin
             #10 sclk = 1;
             mosi = fake_data[7 - i];
@@ -64,7 +58,6 @@ module tb_spi_slave;
         else
             $display("Error: Taken %b, waiting %b", data_write, fake_data);
 
-        // Завершення тесту
         #100;
         $finish;
     end
